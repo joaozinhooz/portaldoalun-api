833313857fd02eace10d1c4e1d46a54927b4dc52
joaozinhooz <j.gui.silva.magalhaes@gmail.com>
2023-10-23 08:46:13 -0300

    configurando banco aqlite|

--- src/config/app_data_source.ts
+++ src/config/app_data_source.ts
@@ -1,13 +1,6 @@
 import { DataSource } from 'typeorm'
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '102030',
-  database: 'quicksalesdb',
-  entities: ['./src/modules/**/models/*.{ts,js}'],
-  // subscribers: [],
-  migrations: ['./src/modules/**/migrations/*.{ts,js}']
+  type: 'sqlite',
+  database: '.database.sqlite',
 })
